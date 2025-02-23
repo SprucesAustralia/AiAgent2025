9e98c6fbe205bfe610297ba9a32d358e6feca2e1
kripesh404 <U1151733@umail.usq.edu.au>
2025-02-23 23:55:54 +1000

    SCRUM-5 Fix prettier.

--- src/models/lead.ts
+++ src/models/lead.ts
@@ -1,19 +1,19 @@
 export enum TypeOfService {
-    OfficeCleaning = 'Office Cleaning',
-    ConstructionSiteCleaning = 'Construction Site Cleaning',
-    IndustrialCleaning = 'Industrial Cleaning',
-    SportsCleaning = 'Sports Cleaning',
-    RealEstateCleaning = 'Real Estate Cleaning',
-    HospitalityCleaning = 'Hospitality Cleaning',
-    CinemaCleaning = 'Cinema Cleaning',
-    AirportCleaning = 'Airport Cleaning'
-  }
+  OfficeCleaning = 'Office Cleaning',
+  ConstructionSiteCleaning = 'Construction Site Cleaning',
+  IndustrialCleaning = 'Industrial Cleaning',
+  SportsCleaning = 'Sports Cleaning',
+  RealEstateCleaning = 'Real Estate Cleaning',
+  HospitalityCleaning = 'Hospitality Cleaning',
+  CinemaCleaning = 'Cinema Cleaning',
+  AirportCleaning = 'Airport Cleaning'
+}
 
 export interface Lead {
-    id: number;
-    fullname: string;
-    email: string;
-    phone: string;
-    typeOfService: TypeOfService;
-    createdAt: string;
+  id: number;
+  fullname: string;
+  email: string;
+  phone: string;
+  typeOfService: TypeOfService;
+  createdAt: string;
 }
